fa0ec458ad691377cea1f3cd5e9c554a8b4600b1
Adam <adamh93@verizon.net>
2017-01-30 21:44:45 -0500

    fix: Recreate the cat menu in the previous versions using data binding, change cat picture for each cat name that is clicked in the menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+var initialCats = [
+  {
+    id: 0,
+    name: "Xuxa",
+    imgSrc: "img/xuxa.jpg",
+    clickCount: 0,
+    nicknames: ['Tabs', 'Lil T', '4 spaces', 'Tabby Tab Tab Tab', 'Tabbin']
+  },
+  {
+    id: 1,
+    name: "Chewie",
+    imgSrc: "img/chewie.jpg",
+    clickCount: 0,
+    nicknames: ['Chewbacca']
+  },
+  {
+    id: 2,
+    name: "Gato",
+    imgSrc: "img/gato.jpg",
+    clickCount: 0,
+    nicknames: ['Cat']
+  },
+  {
+    id: 3,
+    name: "Mittens",
+    imgSrc: "img/mittens.jpg",
+    clickCount: 0,
+    nicknames: ['Gloves']
+  },
+  {
+    id: 4,
+    name: "Fluffy",
+    imgSrc: "img/fluffy.jpg",
+    clickCount: 0,
+    nicknames: ['Fluffy Mufficans']
+  }
+];
+
 var Cat = function(data){
   this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
@@ -13,7 +51,6 @@ var Cat = function(data){
 
     if (clicks < 0){
       return title = 'New Born';
-      console.log("New Born")
     }
     else if (clicks < 10){
       return title = 'Infant';
@@ -37,17 +74,26 @@ var Cat = function(data){
 
 var ViewModel = function(){
   var self = this;    //--- referencing the ViewModel itself, thus the outer this outside of the div of where we applied the 'with data-bind'
-  this.currentCat = ko.observable( new Cat({
-    clickCount: 0,
-    name: 'Tabby',
-    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-    nicknames: ['Tabs', 'Lil T', '4 spaces', 'Tabby Tab Tab Tab', 'Tabbin']
-  }) );
+
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem){
+    self.catList.push( new Cat(catItem) );
+  });
+
+  this.currentCat = ko.observable( this.catList()[0] );
+
+  console.log(this.catList()[1]);
+
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
     //self.currentCat().clickCount(self.currentCat().clickCount() + 1);  --using self allows us to using currentCat() when referencing currentCat in conjunction using 'with data-bind'
   };
-};
+
+  this.setCat = function(clickedCat){
+    self.currentCat(clickedCat);
+  };
+}
 
 ko.applyBindings(new ViewModel());
